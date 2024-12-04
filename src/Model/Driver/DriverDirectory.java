5b40f46079ec0f78eebd93a7dee0d4ae8d6d6504
Parkar-Arslan <parkar.ar@northeastern.edu>
2024-12-04 18:48:04 -0500

    adding user and driver

--- src/Model/Driver/DriverDirectory.java
+++ src/Model/Driver/DriverDirectory.java
@@ -1,1 +1,30 @@
-public class DriverDirectory {}
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DriverDirectory {
+    private List<Driver> driverList;
+
+    public DriverDirectory() {
+        driverList = new ArrayList<>();
+    }
+
+    public Driver createDriver(String driverId, String name, String licenseNumber) {
+        Driver newDriver = new Driver(driverId, name, licenseNumber);
+        driverList.add(newDriver);
+        return newDriver;
+    }
+
+    public List<Driver> getDrivers() {
+        return driverList;
+    }
+
+    public Driver findDriverById(String driverId) {
+        for (Driver driver : driverList) {
+            if (driver.getDriverId().equals(driverId)) {
+                return driver;
+            }
+        }
+        return null; // No driver found
+    }
+}
