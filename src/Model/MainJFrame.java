b2945764764b8e5db63a553ab2c250c464f93464
KanishkT04 <tawde.k@northeastern.edu>
2024-12-04 19:49:39 -0500

    added mainjframe

--- src/Model/MainJFrame.java
+++ src/Model/MainJFrame.java
@@ -2,18 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
  */
-package UI;
+package Model;
 
 /**
  *
  * @author tawde
  */
-public class GarbageCollectionJPanel extends javax.swing.JPanel {
+public class MainJFrame extends javax.swing.JPanel {
 
     /**
      * Creates new form GarbageCollectionJPanel
      */
-    public GarbageCollectionJPanel() {
+    public MainJFrame() {
         initComponents();
     }
 
