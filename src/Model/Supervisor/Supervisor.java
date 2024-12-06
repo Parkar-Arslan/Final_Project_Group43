1cd8788a086097f1ea058c1b09012937649e323a
Parkar-Arslan <parkar.ar@northeastern.edu>
2024-12-06 16:11:45 -0500

    Update Supervisor.java

--- src/Model/Supervisor/Supervisor.java
+++ src/Model/Supervisor/Supervisor.java
@@ -8,6 +8,32 @@ package Model.Supervisor;
  *
  * @author arslanparkar
  */
+
+
+
 public class Supervisor {
-    
+    private String supervisorId;
+    private String name;
+
+    public Supervisor(String supervisorId, String name) {
+        this.supervisorId = supervisorId;
+        this.name = name;
+    }
+
+    // Getters and Setters
+    public String getSupervisorId() {
+        return supervisorId;
+    }
+
+    public void setSupervisorId(String supervisorId) {
+        this.supervisorId = supervisorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
